e9087e921eceec48d0df006700a7b50e2ecd76bb
Aaron Cicali <aaron.cicali@gmail.com>
2015-01-13 19:13:17 +0000

    tabs to spaces

--- src/Message/AIMResponse.php
+++ src/Message/AIMResponse.php
@@ -16,88 +16,88 @@ class AIMResponse extends AbstractResponse
         $this->request = $request;
         $temp = explode('|,|', substr($data, 1, -1));
 
-		$response_fields = array(
-			'Response Code',
-			'Response Subcode',
-			'Response Reason Code',
-			'Response Reason Text',
-			'Authorization Code',
-			'AVS Response',
-			'Transaction ID',
-			'Invoice Number',
-			'Description',
-			'Amount',
-			'Method',
-			'Transaction Type',
-			'Customer ID',
-			'First Name',
-			'Last Name',
-			'Company',
-			'Address',
-			'City',
-			'State',
-			'ZIP Code',
-			'Country',
-			'Phone',
-			'Fax',
-			'Email Address',
-			'Ship To First Name',
-			'Ship To Last Name',
-			'Ship To Company',
-			'Ship To Address',
-			'Ship To City',
-			'Ship To State',
-			'Ship To ZIP Code',
-			'Ship To Country',
-			'Tax',
-			'Duty',
-			'Freight',
-			'Tax Exempt',
-			'Purchase Order Number',
-			'MD5 Hash',
-			'Card Code Response',
-			'Cardholder Authentication Verification Response',
-			'Account Number',
-			'Card Type',
-			'Split Tender ID',
-			'Requested Amount',
-			'Balance On Card'
-		);
+        $response_fields = array(
+            'Response Code',
+            'Response Subcode',
+            'Response Reason Code',
+            'Response Reason Text',
+            'Authorization Code',
+            'AVS Response',
+            'Transaction ID',
+            'Invoice Number',
+            'Description',
+            'Amount',
+            'Method',
+            'Transaction Type',
+            'Customer ID',
+            'First Name',
+            'Last Name',
+            'Company',
+            'Address',
+            'City',
+            'State',
+            'ZIP Code',
+            'Country',
+            'Phone',
+            'Fax',
+            'Email Address',
+            'Ship To First Name',
+            'Ship To Last Name',
+            'Ship To Company',
+            'Ship To Address',
+            'Ship To City',
+            'Ship To State',
+            'Ship To ZIP Code',
+            'Ship To Country',
+            'Tax',
+            'Duty',
+            'Freight',
+            'Tax Exempt',
+            'Purchase Order Number',
+            'MD5 Hash',
+            'Card Code Response',
+            'Cardholder Authentication Verification Response',
+            'Account Number',
+            'Card Type',
+            'Split Tender ID',
+            'Requested Amount',
+            'Balance On Card'
+        );
 
-		$response = array();
+        $response = array();
 
-		foreach($response_fields as $field)
-		{
-			$response[$field] = array_shift($temp);
-		}
+        foreach($response_fields as $field)
+        {
+            $response[$field] = array_shift($temp);
+        }
 
-		$response_codes = array(
-			1 => 'Approved',
-			2 => 'Declined',
-			3 => 'Error',
-			4 => 'Held for Review'
-		);
+        $response_codes = array(
+            1 => 'Approved',
+            2 => 'Declined',
+            3 => 'Error',
+            4 => 'Held for Review'
+        );
 
-		$avs_response_codes = array(
-			'A' => 'Address (Street) matches, ZIP does not',
-			'B' => 'Address information not provided for AVS check',
-			'E' => 'AVS error',
-			'G' => 'Non-U.S. Card Issuing Bank',
-			'N' => 'No Match on Address (Street) or ZIP',
-			'P' => 'AVS not applicable for this transaction',
-			'R' => 'Retry?System unavailable or timed out',
-			'S' => 'Service not supported by issuer',
-			'U' => 'Address information is unavailable',
-			'W' => 'Nine digit ZIP matches, Address (Street) does not',
-			'X' => 'Address (Street) and nine digit ZIP match',
-			'Y' => 'Address (Street) and five digit ZIP match',
-			'Z' => 'Five digit ZIP matches, Address (Street) does not'
-		);
+        $avs_response_codes = array(
+            'A' => 'Address (Street) matches, ZIP does not',
+            'B' => 'Address information not provided for AVS check',
+            'E' => 'AVS error',
+            'G' => 'Non-U.S. Card Issuing Bank',
+            'N' => 'No Match on Address (Street) or ZIP',
+            'P' => 'AVS not applicable for this transaction',
+            'R' => 'Retry?System unavailable or timed out',
+            'S' => 'Service not supported by issuer',
+            'U' => 'Address information is unavailable',
+            'W' => 'Nine digit ZIP matches, Address (Street) does not',
+            'X' => 'Address (Street) and nine digit ZIP match',
+            'Y' => 'Address (Street) and five digit ZIP match',
+            'Z' => 'Five digit ZIP matches, Address (Street) does not'
+        );
 
-		$response['Response Code'] = $response_codes[$response['Response Code']];
-		$response['AVS Response'] = $avs_response_codes[$response['AVS Response']];
+        $response['Response Code'] = $response_codes[$response['Response Code']];
+        $response['AVS Response'] = $avs_response_codes[$response['AVS Response']];
 
-		$this->data = $response;
+        $this->data = $response;
 
         if (count($this->data) < 10) {
             throw new InvalidResponseException();
