ac397199d765b52654cf88722f278c5d1b9cf6ed
Jason Judge <jason.judge@consil.co.uk>
2015-02-06 13:46:51 +0000

    Dev snapshot

--- src/Message/DPMCompleteAuthorizeResponse.php
+++ src/Message/DPMCompleteAuthorizeResponse.php
@@ -3,6 +3,7 @@
 namespace Omnipay\AuthorizeNet\Message;
 
 use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RedirectResponseInterface;
 
 /**
  * Authorize.Net DPM Complete Authorize Response
@@ -13,28 +14,72 @@ use Omnipay\Common\Message\AbstractResponse;
  * We may want to return to the success page, the failed page or the retry
  * page (so the user can correct the form).
  */
-class DPMCompleteAuthorizeResponse extends SIMCompleteAuthorizeResponse // TOOD: redirect
+class DPMCompleteAuthorizeResponse extends SIMCompleteAuthorizeResponse implements RedirectResponseInterface
 {
-    const RESPONSE_CODE_APPROVED = '1';
-    const RESPONSE_CODE_DECLINED = '2';
-    const RESPONSE_CODE_ERROR = '3';
-    const RESPONSE_CODE_REVIEW = '4';
+    const RESPONSE_CODE_APPROVED    = '1';
+    const RESPONSE_CODE_DECLINED    = '2';
+    const RESPONSE_CODE_ERROR       = '3';
+    const RESPONSE_CODE_REVIEW      = '4';
 
     public function isSuccessful()
     {
         return isset($this->data['x_response_code']) && static::RESPONSE_CODE_APPROVED === $this->data['x_response_code'];
     }
 
-    public function getTransactionReference()
+    /**
+     * If there is an error in the form, then the user should be able to go back
+     * to the form and give it another shot.
+     */
+    public function isError()
     {
-        return isset($this->data['x_trans_id']) ? $this->data['x_trans_id'] : null;
+        return isset($this->data['x_response_code']) && static::RESPONSE_CODE_ERROR === $this->data['x_response_code'];
     }
-
-    // TODO: getCode()?
-    // TODO: redirect details (need "reptry" URL too).
 
     public function getMessage()
     {
-        return isset($this->data['x_response_reason_text']) ? $this->data['x_response_reason_text'] : null;
+        return parent::getReasonCode() . '|' . parent::getMessage();
+    }
+
+    /**
+     * We are in the callback, and we MUST return a HTML fragment to do a redirect.
+     * All headers we may return are discarded by the gateway, so we cannot use
+     * the "Location:" header.
+     */
+    public function isRedirect()
+    {
+        return true;
+    }
+
+    /**
+    * We default here to POST because the default redirect mechanism
+    * in Omnipay Common only generates a HTML snippet for POST and not
+    * GET.
+    * TODO: We could fix that here so both GET and POST can be supported.
+    * Our fix should also include the "form data" with the URL.
+    */
+    public function getRedirectMethod()
+    {
+        return 'POST';
+    }
+
+    /**
+     * We probably do not require any redirect data, if the incomplete transaction
+     * is still in the user's session and we can inspect the results from the saved
+     * transaction in the database. We cannot send the result through the redirect
+     * unless it is hashed in some way so the authorisation result cannot be faked.
+     */
+    public function getRedirectData()
+    {
+        return array();
+    }
+
+    /**
+     * The cancel URL is never handled here - that is a direct link from the gateway.
+     * The best approach is to have just one redirect URL, and once there, check the
+     * result of the authorisation in the database (assuming it has been saved in the
+     * callback) and take action from there.
+     */
+    public function getRedirectUrl()
+    {
     }
 }
