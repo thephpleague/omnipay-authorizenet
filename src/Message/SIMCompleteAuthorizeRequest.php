6661d9b179a6c86ab512937918ee1bffb06c8727
Jason Judge <jason.judge@consil.co.uk>
2015-03-07 12:30:16 +0000

    Some notes. Will raise a ticket against this.

--- src/Message/SIMCompleteAuthorizeRequest.php
+++ src/Message/SIMCompleteAuthorizeRequest.php
@@ -19,7 +19,9 @@ class SIMCompleteAuthorizeRequest extends AbstractRequest
     }
 
     /**
-     * CHECKME: DPM uses the transactionReference in the hash, not the transactionID.
+     * CHECKME: should this be the transactionReference in the hash, not the transactionId?
+     * The transaction reference and the amount are both sent by the remote gateway (x_trans_id
+     * and x_amount) and it is those that should be checked against.
      */
     public function getHash()
     {
