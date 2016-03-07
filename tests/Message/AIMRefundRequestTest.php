e54383a28afb4df46220b3b42ff35926f0e88ee8
Mike Almond <almond@raceroster.com>
2016-03-07 10:31:39 -0500

    [AIMRefundRequest] Add test when no credit card expiry dates are set

--- tests/Message/AIMRefundRequestTest.php
+++ tests/Message/AIMRefundRequestTest.php
@@ -29,5 +29,24 @@ class AIMRefundRequestTest extends TestCase
         $this->assertSame('60O2UZ', $data['x_trans_id']);
         $this->assertSame($card['number'], $data['x_card_num']);
         $this->assertSame('12.00', $data['x_amount']);
+
+        $this->assertArrayHasKey('x_exp_date', $data);
+    }
+
+    public function testRefundWithSimplifiedCard()
+    {
+        $simplifiedCard = array(
+            'firstName' => 'Example',
+            'lastName' => 'User',
+            'number' => '1111',
+        );
+
+        $this->request->setCard($simplifiedCard);
+
+        $data = $this->request->getData();
+
+        $this->assertSame($simplifiedCard['number'], $data['x_card_num']);
+
+        $this->assertArrayNotHasKey('x_exp_date', $data);
     }
 }
