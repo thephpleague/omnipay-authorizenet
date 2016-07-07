bfd7b848ff7d5556a66ee81ced575e414624a52e
Alberto <alberto1el@hotmail.com>
2016-07-07 10:26:23 -0700

    Update CIMDeletePaymentProfileResponse.php
    Changed the parent class, since there is no need for additional methods

--- src/Message/CIMDeletePaymentProfileResponse.php
+++ src/Message/CIMDeletePaymentProfileResponse.php
@@ -5,12 +5,8 @@ namespace Omnipay\AuthorizeNet\Message;
 /**
  * Authorize.Net CIM Delete payment profile Response
  */
-class CIMDeletePaymentProfileResponse extends CIMCreatePaymentProfileResponse
+class CIMDeletePaymentProfileResponse extends CIMAbstractResponse
 {
     protected $responseType = 'deleteCustomerPaymentProfileResponse';
 
-    public function getCustomerPaymentProfileId()
-    {
-        return $this->request->getCustomerPaymentProfileId();
-    }
 }
