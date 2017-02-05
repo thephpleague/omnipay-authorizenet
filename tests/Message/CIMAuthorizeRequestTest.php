36c567c5a5c51912789753e72453252213a9722a
Jason Judge <jason.judge@consil.co.uk>
2017-02-05 23:22:00 +0000

    Issue #69 Include invoiceNumber if supplied in CIM Authorize.

--- tests/Message/CIMAuthorizeRequestTest.php
+++ tests/Message/CIMAuthorizeRequestTest.php
@@ -16,6 +16,7 @@ class CIMAuthorizeRequestTest extends TestCase
             array(
                 'cardReference' => '{"customerProfileId":"28972085","customerPaymentProfileId":"26317841","customerShippingAddressId":"27057151"}',
                 'amount' => '12.00',
+                'invoiceNumber' => '00001234',
                 'description' => 'Test authorize transaction'
             )
         );
@@ -29,6 +30,7 @@ class CIMAuthorizeRequestTest extends TestCase
         $this->assertEquals('28972085', $data->transactionRequest->profile->customerProfileId);
         $this->assertEquals('26317841', $data->transactionRequest->profile->paymentProfile->paymentProfileId);
         $this->assertEquals('27057151', $data->transactionRequest->profile->shippingProfileId);
+        $this->assertEquals('00001234', $data->transactionRequest->order->invoiceNumber);
         $this->assertEquals('Test authorize transaction', $data->transactionRequest->order->description);
     }
 }
