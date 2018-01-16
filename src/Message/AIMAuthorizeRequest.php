eb59611f0d97ea42eadc925d78c69325b39a248e
Sam L <samlitowitz@arts-people.com>
2018-01-16 09:13:04 -0500

    Seperate retail into deviceType and marketType

--- src/Message/AIMAuthorizeRequest.php
+++ src/Message/AIMAuthorizeRequest.php
@@ -3,6 +3,7 @@
 namespace Omnipay\AuthorizeNet\Message;
 
 use Omnipay\Common\CreditCard;
+use Omnipay\Common\Exception\InvalidRequestException;
 
 /**
  * Authorize.Net AIM Authorize Request
@@ -56,30 +57,52 @@ class AIMAuthorizeRequest extends AIMAbstractRequest
 
     protected function addRetail(\SimpleXMLElement $data)
     {
-        $retail = $this->getParameter('retail');
-        if (isset($retail)) {
-            $data->transactionRequest->retail = $this->getParameter('retail');
+        $deviceType = $this->getDeviceType();
+        $marketType = $this->getMarketType();
+
+        if (!isset($deviceType) && !isset($marketType)) {
+            return;
         }
+
+        if (!isset($deviceType) && isset($marketType)) {
+            throw new InvalidRequestException();
+        }
+
+        if (isset($deviceType) && !isset($marketType)) {
+            $marketType = "2";
+        }
+
+        if (!in_array($deviceType, [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" ])) {
+            throw new InvalidRequestException();
+        }
+
+        if (!in_array($marketType, [ "0", "1", "2" ])) {
+            throw new InvalidRequestException();
+        }
+
+        $data->transactionRequest->retail = [
+            'marketType' => $marketType,
+            'deviceType' => $deviceType,
+        ];
     }
 
-    /**
-     * Gets the retail data of the request from the gateway.
-     *
-     * @return array
-     */
-    public function getRetail()
+    public function getDeviceType()
     {
-        return $this-getRetail();
+        return $this->getParameter('deviceType');
     }
 
-    /**
-     * Sets the retail data in this request
-     *
-     * @param array $value An associative arrray with keys marketType and/or deviceType
-     * @return AIMAuthorizeRequest Provides a fluent interface
-     */
-    public function setRetail($value)
+    public function setDeviceType($value)
     {
-        return $this->setParameter('retail', $value);
+        return $this->setParameter('deviceType', $value);
+    }
+    
+    public function getMarketType()
+    {
+        return $this->getParameter('marketType');
+    }
+
+    public function setMarketType($value)
+    {
+        return $this->setParameter('marketType', $value);
     }
 }
